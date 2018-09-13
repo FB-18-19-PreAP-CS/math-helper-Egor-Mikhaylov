fba711d612a7ea93264f788db7b4b7b13e08b6de
MIKHAYLOV.EGOR <2201136@fbisd.flourbluffschools.net>
2018-09-13 13:48:25 -0500

    fix fib function

--- math_helper_mikhaylove.py
+++ math_helper_mikhaylove.py
@@ -13,12 +13,20 @@ def main():
     print("Here is a list of all of the five formulas which are availiable in Math Helper:")
     while True:
         print("1. Pythagorean Theorem")
+        print("2. Slope")
         formula = int(input("Please select the formula: "))
         if formula == 1:
             #Formula 1: Pythagorean Theorem
             x = float(input("1st Side Length: "))
             y = float(input("2nd Side Length: "))
             print("The hypotenuse is: {:0.2f}".format(pyth(x, y)))
+        if formula == 2:
+            #Formula 2: Slope
+            x1 = float(input("X value for the first coordinate: "))
+            y1 = float(input("Y value for the first coordinate: "))
+            x2 = float(input("X value for the second coordinate: "))
+            y1 = float(input("Y value for the second coordinate: "))
+            print("The slope is: {:0.2f}".format(slope(x1, y1, x2, y2)))
         else:
             print("That is not an option.")
         
@@ -34,5 +42,12 @@ def pyth(x, y):
     answer = math.sqrt(x**2 + y**2)
     return answer
 
+def slope(x1, y1, x2, y2):
+    ''' Returns the slope for two pairs of
+    coordinates.'''
+    answer = (y2-y1)/(x2-x1)
+    return answer
+    
+    
 if __name__ == "__main__":
     main()
